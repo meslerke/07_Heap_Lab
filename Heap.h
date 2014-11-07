f5c8665c98d88f47265c9a50538cb0eb7926b11e
meslerke <meslerke@miamioh.edu>
2014-11-07 17:00:52 -0500

    Worked on the grow(), add(), getNumItems(), and bubbleUp() methods and the constructor and destructor

--- Heap.h
+++ Heap.h
@@ -5,7 +5,7 @@
 #define START_SIZE 10
 
 /*
- * This is a priority queue, which means that as items are removed in order
+ * This is a priority queue, which means that items are removed in order
  * of decreasing priority.
  *
  * 
@@ -57,44 +57,78 @@ private:
 
 template<class Pri, class T>
 Heap<Pri, T>::Heap(){
-	//TODO
+	arrSize = START_SIZE;
+	numItems = 0;
+	backingArray = new std::pair<Pri, T>[START_SIZE];
 }
 
 template<class Pri, class T>
 Heap<Pri, T>::~Heap(){
-	//TODO
+	delete[] backingArray;
 }
 
 template<class Pri, class T>
 void Heap<Pri, T>::grow(){
-	//TODO
+	std::pair<Pri, T>* oldBackingArray = backingArray;
+	arrSize *= 2;
+	backingArray = new std::pair<Pri, T>[arrSize];
+	for (int i = 0; i < arrSize / 2; i++) {
+		backingArray[i] = oldBackingArray[i];
+	}
+
+	delete[] oldBackingArray;
+
 }
 
 template<class Pri, class T>
 void Heap<Pri, T>::add(std::pair<Pri, T> toAdd){
-	//TODO
+	numItems++;
+	if (numItems >= arrSize) {
+		grow();
+	}
+	backingArray[numItems - 1] = toAdd;
+	bubbleUp(numItems - 1);
+	trickleDown(numItems - 1);
 }
 
+//Check the item at index, and make sure it is in the right place.
+// If not, swap it up the "tree" of the heap until you find the right
+// place
 template<class Pri, class T>
 void Heap<Pri, T>::bubbleUp(unsigned long index){
-	//TODO
+	if (backingArray[index] >= backingArray[(index - 1) / 2]) { //if the element at this index is >= than its parent
+		return;
+	}
+	while (backingArray[index] < backingArray[(index - 1) / 2]) { //while this element is smaller than its parent
+		std::pair<Pri, T> tmp = backingArray[index];				//swap the values at each index
+		backingArray[index] = backingArray[(index - 1) / 2];
+		backingArray[(index - 1) / 2] = tmp;
+	}
 }
 
+//Check the item at index, and make sure it is in the right place.
+// If not, swap it down the "tree" of the heap until you find the right
+// place
 template<class Pri, class T>
 void Heap<Pri, T>::trickleDown(unsigned long index){
 	//TODO
 }
 
+//Remove the item with lowest priority, and return it
+//If the queue is empty, throw a string exception
 template<class Pri, class T>
 std::pair<Pri, T> Heap<Pri, T>::remove(){
-	//TODO
+	if (numItems == 0) {
+		std::cout << "ERROR: Queue is empty and cannot remove" << std::endl;
+		//return tmp;
+	}
+	//backingArray[numItems - 1] = NULL;
 	std::pair<Pri, T> tmp;
 	return tmp;
 }
 
 template<class Pri, class T>
 unsigned long Heap<Pri, T>::getNumItems(){
-	//TODO
-	return 0;
+	return numItems;
 }
 
