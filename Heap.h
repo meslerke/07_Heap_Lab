8d0ed630540873e1c78f1b34c649362bb77436ac
meslerke <meslerke@miamioh.edu>
2014-12-06 18:03:41 -0500

    Fixed code to run and pass tests

--- Heap.h
+++ Heap.h
@@ -86,19 +86,10 @@ void Heap<Pri, T>::add(std::pair<Pri, T> toAdd){
 	if (numItems >= arrSize) {
 		grow();
 	}
-	
 	backingArray[numItems - 1] = toAdd;
-	
-	if (numItems == 1) {
-		return;
-	}
-	
 	bubbleUp(numItems - 1);
 }
 
-//Check the item at index, and make sure it is in the right place.
-// If not, swap it up the "tree" of the heap until you find the right
-// place
 template<class Pri, class T>
 void Heap<Pri, T>::bubbleUp(unsigned long index){
 	if (index == 0) {
@@ -107,54 +98,51 @@ void Heap<Pri, T>::bubbleUp(unsigned long index){
 	if (backingArray[index] >= backingArray[(index - 1) / 2]) { //if the element at this index is >= than its parent
 		return;
 	}
-		std::pair<Pri, T> tmp = backingArray[index];				//swap the values at each index
-		backingArray[index] = backingArray[(index - 1) / 2];
-		backingArray[(index - 1) / 2] = tmp;
+	swap(backingArray[index], backingArray[(index - 1) / 2]);
 		return bubbleUp((index - 1) / 2);
 }
 
-//Check the item at index, and make sure it is in the right place.
-// If not, swap it down the "tree" of the heap until you find the right
-// place
 template<class Pri, class T>
 void Heap<Pri, T>::trickleDown(unsigned long index){
-	if (backingArray[index] <= backingArray[2 * index + 1] && backingArray[index] <= backingArray[2 * index + 2]) {
-		return;
-	}
-		if (backingArray[2 * index + 1] < backingArray[2 * index + 2]) { //if the left child is smaller than the right child
-			std::pair<Pri, T> tmp = backingArray[index];				//swap the index with the left child
-			backingArray[index] = backingArray[2 * index + 1];
-			backingArray[2 * index + 1] = tmp;
-			return trickleDown(2 * index + 1);
+	//My dad helped me with the implementation of this method
+	bool done;
+	do{
+		done = false;
+		int leftChild = (2 * index) + 1;
+		int rightChild = (2 * index) + 2;
+
+		if (leftChild < numItems && backingArray[index].first > backingArray[leftChild].first){
+			if (backingArray[leftChild].first < backingArray[rightChild].first){	//swap with smaller child
+				swap(backingArray[index], backingArray[leftChild]);
+				index = leftChild;
+				done = true;
+			} else{
+				swap(backingArray[index], backingArray[rightChild]);
+				index = rightChild;
+				done = true;
+			}
+		} else{ //index is not larger than leftChild so now check if it's larger than rightChild
+			if (rightChild < numItems && backingArray[index].first > backingArray[rightChild].first){
+				swap(backingArray[index], backingArray[leftChild]);
+				index = leftChild;
+				done = true;
+			} 
+			else{ //index is where it is supposed to be
+				done = true;
+			}
 		}
-		else {
-			std::pair<Pri, T> tmp = backingArray[index];				//else, swap with the right child
-			backingArray[index] = backingArray[2 * index + 2];
-			backingArray[2 * index + 2] = tmp;
-			return trickleDown(2 * index + 2);
-		}
+	} while (done == false);
 }
 
-//Remove the item with lowest priority, and return it
-//If the queue is empty, throw a string exception
 template<class Pri, class T>
 std::pair<Pri, T> Heap<Pri, T>::remove(){
-	if (numItems == 0) {
+	if(numItems < 1){
 		std::cout << "ERROR: Queue is empty and cannot remove" << std::endl;
 	}
-	
-	if (numItems > 1) {
-	std::pair<Pri, T> tmp = backingArray[0];				//swap root with bottom
+	std::pair<Pri, T> ret = backingArray[0];
 	backingArray[0] = backingArray[numItems - 1];
-	backingArray[numItems - 1] = tmp;
-
+	numItems--;
 	trickleDown(0);
-	}
-	
-	numItems--;
-	std::pair<Pri, T> ret = backingArray[numItems - 1];
-	backingArray[numItems - 1].first = NULL;
-	numItems--;
 	return ret;
 }
 
